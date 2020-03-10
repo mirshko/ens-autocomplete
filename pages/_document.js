b9a4221a7233cd84973195e8f926206ca8c3828c
Jeff Reiner <theundeadghost@gmail.com>
2020-03-10 10:33:40 +0100

    :chart_with_upwards_trend: Adding fathom

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import { FATHOM_SITE_ID } from "../lib/fathom";
+
+export default class extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(f, a, t, h, o, m){
+              a[h]=a[h]||function(){
+              (a[h].q=a[h].q||[]).push(arguments)
+              };
+              o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+              o.async=1; o.src=t; o.id='fathom-script';
+              m.parentNode.insertBefore(o,m)
+              })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+              fathom('set', 'siteId', '${FATHOM_SITE_ID}');
+              fathom('trackPageview');
+          `
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
